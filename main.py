531b034346b3f4d1650ebb7ac924112267642af2
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-08-01 15:33:10 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,13 @@ class mcfunction():
         mcpath = (os.environ["APPDATA"].replace("Roaming","")+"Local/"+"packages/")
 
         mcpath = mcpath.replace("\\","/")
-
-
-
-        i=0
-
+        
         for name in glob.glob(f'{mcpath}*'):
             name = (name.replace("\\","/"))
             name = (name.replace(mcpath,""))
             name_path = "MinecraftUWP" in name
             if(name_path==True):
                 break
-            i=i+1
 
         mcpath = (mcpath+name+"/LocalState/games/com.mojang/")
 
